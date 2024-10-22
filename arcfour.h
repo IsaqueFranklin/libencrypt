6961b8b838b4b47603fc2ff8064536963ef9c17a
Isaque Franklin <isaquefrankli@gmail.com>
2024-10-21 22:11:22 -0300

    More stuff.

--- arcfour.h
+++ arcfour.h
@@ -12,3 +12,5 @@ struct s_arcfour {
 typedef unsigned char int8;
 typedef unsigned short int int16;
 typedef unsigned int int32;
+
+Arcfour *rc4init(int8*, int16)
--- example.c
+++ example.c
@@ -6,8 +6,11 @@ int main(void);
 
 int main() {
   Arcfour *rc4;
+  int16 skey, stext;
   char *key, *from, *encrypted, *decrypted;
 
   key = "tomatoes"; /* 8 bits -> 2048 bits */
+  skey = strlen(key);
   from = "Shall I compare thee to a summer's day?";
+  stext = strlen(from);
 }
